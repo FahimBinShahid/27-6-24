fb8644a7e31dc37ae8f9bd2b4988d334c887491a
fahimBinShahid <fahimbs.mc2@gmail.com>
2024-06-30 11:54:45 +0600

    Update MobilePhone.java

--- Day 10/Mobile Phone/src/dev/lpa/MobilePhone.java
+++ Day 10/Mobile Phone/src/dev/lpa/MobilePhone.java
@@ -1,77 +1,67 @@
 package dev.lpa;
+
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Arrays;
-import java.util.Objects;
 
 public class MobilePhone {
-    public static void main(String[] args) {
+
+    private ArrayList<com.learnJava.Contact> contactList = new ArrayList<com.learnJava.Contact>();
+
+    // Getters and setters
+    public ArrayList<com.learnJava.Contact> getContacts() {
+        return contactList;
     }
 
-        private String myNumber;
-        private ArrayList<String> myContacts;
-
-
-    public MobilePhone(String myNumber) {
-            this.myNumber = myNumber;
-
-            this.myContacts = new ArrayList<>();
-            myContacts.add(new String("Rebeca", "01234567890"));
-            myContacts.add(new String("Soler", "01987654321"));
-        }
-
-    public boolean addNewContact(String contact){
-        for(String existing : myContacts){
-            if(existing.equals(contact))
-                return false;
-        }
-        myContacts.add(contact);
-        return true;
+    public void setContacts(ArrayList<com.learnJava.Contact> contacts) {
+        this.contactList = contacts;
     }
-    public boolean updateContact(String oldContact, String newContact){
-        for(String existing : myContacts){
-            if (existing.equals(oldContact)) {
-                myContacts.set(myContacts.indexOf(oldContact),newContact);
-                return true;
+
+    // Methods
+    public void printListOfContacts() {
+        if(contactList.size() == 0) {
+            System.out.println("List of contacts is empty");
+        } else {
+            System.out.println("/*--- List of contacts ---*/");
+            for(int i = 0; i < contactList.size(); i++) {
+                System.out.println(contactList.get(i).getName() + ": " + contactList.get(i).getNumber());
             }
         }
-        return false;
     }
-    public boolean removeContact(String contact){
-        for(String existing : myContacts){
-            if(existing.equals(contact)){
-                myContacts.remove(String existing);
-                return true;
-            }
+
+    public void addContact(com.learnJava.Contact contact) {
+        contactList.add(contact);
+        System.out.println("Contact added");
+    }
+
+    public void updateContact(String name, com.learnJava.Contact newContact) {
+        int position = findContact(name);
+        if(position < 0) {
+            System.out.println("Contact not found");
         }
-        return false;
+        contactList.set(position, newContact);
+        System.out.println("Contact updated");
     }
-    private int findContact(String contact){
-        return myContacts.indexOf(contact);
+
+    public void removeContact(String name) {
+        int position = findContact(name);
+        if(position < 0) {
+            System.out.println("Contact not found");
+        }
+        contactList.remove(position);
+        System.out.println("Contact deleted");
     }
-    private int findContact(String stringInput){
-        for(String contact : myContacts){
-            if((contact.getName).equals(stringInput)){
-                return contact.indexOf(contact);
+
+    public int findContact(com.learnJava.Contact contact) {
+        return contactList.indexOf(contact);
+    }
+
+    public int findContact(String contactName) {
+        for(int i = 0; i < contactList.size(); i++) {
+            com.learnJava.Contact contact = contactList.get(i);
+            if(contact.getName().toLowerCase().equals(contactName.toLowerCase())) {
+                return i;
             }
         }
         return -1;
-    }
-    public String queryContact(String stringInput){
-        int index = findContact(stringInput);
-        if(index >= 0){
-            return myContacts.get(index);
-        }else{
-            return null;
-        }
-    }
-    public void printContact(){
-        System.out.println("Contact List:");
-        for(int i = 0; i< myContacts.size(); i++){
-            System.out.println((i+1) + ". " +
-                    (myContacts.get(i)).getName() + "->" +
-                    (myContacts.get(i).getPhoneNumber()) );
-        }
     }
 
 }
